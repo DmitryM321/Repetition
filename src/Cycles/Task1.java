053e7d1a4643738df30b1ce02fe4b648df8a9e9d
A1 <A1@mail.ind>
2023-06-12 11:10:16 +0500

    Task1

---
+++ src/Cycles/Task1.java
@@ -0,0 +1,9 @@
+package Cycles;
+// С помощью цикла for выведите в консоль все числа от 1 до 10.
+public class Task1 {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 10; i++){
+            System.out.print(i + " ");
+        }
+    }
+}
