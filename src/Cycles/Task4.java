4b2c6ab93028cae25bd2bec3ba3a50930a2e3546
A1 <A1@mail.ind>
2023-06-12 11:19:17 +0500

    Task4

---
+++ src/Cycles/Task4.java
@@ -0,0 +1,9 @@
+package Cycles;
+// Выведите в консоль все числа от 10 до −10 от бо́льшего числа к меньшему.
+public class Task4 {
+    public static void main(String[] args) {
+         for (int i = 10; i >= -10; i--){
+            System.out.print(i + " ");
+        }
+    }
+}
