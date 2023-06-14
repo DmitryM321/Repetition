6a4cc2386971890a5daa0a0c505f15fc30c6c137
Dmitry M <a89297175356@gmail.com>
2023-06-14 17:19:11 +0500

    Task3

---
+++ src/Arrays/Task3.java
@@ -0,0 +1,30 @@
+package Arrays;
+
+public class Task3 {
+    public static int[] generateRandomArray() {
+        java.util.Random random = new java.util.Random();
+        int[] arr = new int[30];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = random.nextInt(100_000) + 100_000;
+        }
+        return arr;
+    }
+    public static void task2() {
+        int[] arr = generateRandomArray();
+    }
+    public static void main(String[] args) {
+        int[] arr = generateRandomArray();
+        int maxDay = 0;
+        int minDay = 2000000;
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] > maxDay) {
+                maxDay = arr[i];
+            }
+            if (arr[i] < minDay)
+                minDay = arr[i];
+        }
+            System.out.println(maxDay);
+            System.out.println(minDay);
+        }
+    }
+
