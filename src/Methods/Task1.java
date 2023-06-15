f949b768a0bddcf6fa8f1603b21c8cc1262df322
Dmitry M <a89297175356@gmail.com>
2023-06-15 17:22:28 +0500

    Task1

---
+++ src/Methods/Task1.java
@@ -0,0 +1,18 @@
+package Methods;
+import java.util.Scanner;
+
+public class Task1 {
+    public static void leapYear(int a) {
+            if ((a % 400 == 0) || ((a % 4 == 0) && (a % 100 != 0))) {
+                System.out.println(a + " год является високосным");
+            } else {
+                System.out.println(a + " год не является високосным");
+            }
+        }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int year = sc.nextInt();
+        leapYear(year);
+    }
+    }
+
