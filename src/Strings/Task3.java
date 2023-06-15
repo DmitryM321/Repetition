76d6f407d5d4b08d3e4853fa6df3e271d08d96fe
Dmitry M <a89297175356@gmail.com>
2023-06-15 14:58:17 +0500

    Task3

---
+++ src/Strings/Task3.java
@@ -0,0 +1,11 @@
+package Strings;
+
+import java.util.Scanner;
+
+public class Task3 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        System.out.println("Температура воздуха сегодня: " + t + " градусов.");
+    }
+}
