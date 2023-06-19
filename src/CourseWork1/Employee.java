0a5e9529c5861aac00580ede296bb88485f9afca
Dmitry M <a89297175356@gmail.com>
2023-06-19 13:13:27 +0500

    CourseWork

---
+++ src/CourseWork1/Employee.java
@@ -0,0 +1,34 @@
+package CourseWork1;
+
+public class Employee {
+    private String fio;
+    private int department;
+    private double salary;
+    private static int count;
+    private int id = count++;
+
+    public void setDepartment(int department) {
+        this.department = department;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+
+    public String getFio() {
+        return fio;
+    }
+
+    public int getDepartment() {
+        return department;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public int getId() {
+        return id;
+    }
+}
+
