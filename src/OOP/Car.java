f1d8f990977279631a175921782c0ef363a62b52
Dmitry M <a89297175356@gmail.com>
2023-06-26 13:21:29 +0500

    OOP1.3

--- src/OOP/Car.java
+++ src/OOP/Car.java
@@ -8,6 +8,33 @@ public class Car {
     private int year;
     private String country;
 
+    public Car(String brand, String model, double engineVolume, String color, int year, String country) {
+       if(brand == null || brand.isEmpty()) {
+           brand = "default";
+       }
+        if(model == null || model.isEmpty()) {
+            model = "default";
+        }
+        if(country == null || country.isEmpty()) {
+            country = "default";
+        }
+        if(engineVolume <=0 ) {
+            engineVolume = 1.5;
+        }
+        if(color == null || color.isEmpty()) {
+            color = "белый";
+        }
+        if(year <=0 ) {
+            engineVolume = 2000;
+        }
+        this.brand = brand;
+        this.model = model;
+        this.engineVolume = engineVolume;
+        this.color = color;
+        this.year = year;
+        this.country = country;
+    }
+
     @Override
     public String toString() {
         return "Car{" +
@@ -21,47 +48,18 @@ public class Car {
     }
 
     public static void main(String[] args) {
-        Car car1 = new Car();
-        car1.brand = "Lada";
-        car1.model = "Granta";
-        car1.engineVolume = 1.7;
-        car1.color = "желтый";
-        car1.year = 2015;
-        car1.country = "Россия";
+            Car car1 = new Car("Lada", "Granta", 1.7, "желтый", 2015, "Россия");
+            Car car2 = new Car("Audi", "A8 50 L TDI quattro", 3.0, "черный", 2020, "Германия");
+            Car car3 = new Car("BMW", "Z8", 3.0, "черный", 2021, "Германия");
+            Car car4 = new Car("Kia", "Sportage", 2.4, "красный", 2018, "Южная Корея");
+            Car car5 = new Car("", "Avante", -1.6, null, 2016, "Южная Корея");
         System.out.println(car1);
-        Car car2 = new Car();
-        car2.brand = "Audi";
-        car2.model = "A8 50 L TDI quattro";
-        car2.engineVolume = 3.0;
-        car2.color = "черный";
-        car2.year = 2020;
-        car2.country = "Германия";
         System.out.println(car2);
-        Car car3 = new Car();
-        car3.brand = "BMW";
-        car3.model = "Z8";
-        car3.engineVolume = 3.0;
-        car3.color = "черный";
-        car3.year = 2021;
-        car3.country = "Германия";
         System.out.println(car3);
-        Car car4 = new Car();
-        car4.brand = "Kia";
-        car4.model = "Sportage";
-        car4.engineVolume = 2.4;
-        car4.color = "красный";
-        car4.year = 2018;
-        car4.country = "Южная Корея";
         System.out.println(car4);
-        Car car5 = new Car();
-        car5.brand = "Hyundai";
-        car5.model = "Avante";
-        car5.engineVolume = 1.6;
-        car5.color = "оранжевый";
-        car5.year = 2016;
-        car5.country = "Южная Корея";
         System.out.println(car5);
-    }
+
+        }
 }
 
 
