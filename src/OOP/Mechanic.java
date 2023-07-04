4e9ea97465feda8f9f95d568cf09f27a979509c5
Dmitry M <a89297175356@gmail.com>
2023-07-04 12:58:45 +0500

    OOP1.12

--- src/OOP/Mechanic.java
+++ src/OOP/Mechanic.java
@@ -1,7 +1,64 @@
 package OOP;
 
-public class Mechanic {
+import OOP.driver.Driver;
+import OOP.transport.Transport;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Mechanic <T extends Transport> {
     private String fio;
     private String nameCompany;
+    private List<T> transportList;
 
+    public Mechanic(String fio, String nameCompany) {
+        this.fio = fio;
+        this.nameCompany = nameCompany;
+        this.transportList = new ArrayList<>();
+    }
+
+    public void performMaintenance(T transport) {
+        if (!transportList.contains(transport)) {
+            System.out.println("Транспорт не добавлен. Сначала добавьте транспорт через метод addTransport.");
+            return;
+        }
+        System.out.println("Mechanic " + fio + " проводит техобслуживание " + transport.getBrand() + " " + transport.getModel());
+    }
+
+    public void repair(T transport) {
+            if (!transportList.contains(transport)) {
+                System.out.println("Транспорт не добавлен. Сначала добавьте транспорт через метод addTransport.");
+                return;
+            }
+        System.out.println("Mechanic " + fio + " чинит " + transport.getBrand() + " " + transport.getModel());
+    }
+
+    public String getFio() {
+        return fio;
+    }
+
+    public void setFio(String fio) {
+        this.fio = fio;
+    }
+
+    public String getNameCompany() {
+        return nameCompany;
+    }
+
+    public void setNameCompany(String nameCompany) {
+        this.nameCompany = nameCompany;
+    }
+
+    public List<T> getTransportList() {
+        return transportList;
+    }
+
+    public void addTransport(T transport) {
+        transportList.add(transport);
+    }
+
+    public void removeTransport(T transport) {
+        transportList.remove(transport);
+    }
 }
+
--- src/OOP/StartApp.java
+++ src/OOP/StartApp.java
@@ -11,7 +11,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class StartApp {
-    public static void main(String[] args) {пш
+    public static void main(String[] args) {
         List<Bus> buses = new ArrayList<>();
         buses.add(new Bus("Volvo", "9500", 10.0));
         buses.add(new Bus("Mercedes-Benz", "Citaro", 12.0));
@@ -20,21 +20,32 @@ public class StartApp {
         List<Truck> trucks = new ArrayList<>();
         trucks.add(new Truck("Scania", "R500", 15.0));
         trucks.add(new Truck("Kia", "FH16", 16.0));
-        trucks.add(new Truck("Chevrolet", "Actros", 13.0));
+        Truck truck2 = new Truck("Chevrolet", "Actros", 13.0);
 
         List<Car> cars = new ArrayList<>();
         cars.add(new Car("Toyota", "Camaro", 2.0));
         cars.add(new Car("Kia", "Corolla", 1.8));
-        cars.add(new Car("Chevrolet", "Sportage", 2.2))
+        Car car2 = new Car("Chevrolet", "Sportage", 2.2);
 
 
-        CarDriver carDriver1 = new CarDriver("Шарль ДеГоль", -22);
-        CarDriver carDriver2 = new CarDriver("Чан Кайши", 12);
-        BusDriver busDriver = new BusDriver("Теодор Рузвельт", 33);
-        TruckDriver truckDriver = new TruckDriver("Уинстон Черчилль", -43);
-        car1.setDriver(carDriver1);
-        car1.checkExperience(carDriver1);
-        truck1.checkExperience(truckDriver);
+        List<CarDriver> carDrivers = new ArrayList<>();
+        carDrivers.add(new CarDriver("Шарль ДеГоль", -22));
+        carDrivers.add(new CarDriver("Чан Кайши", 12));
+        CarDriver carDriver2 = new CarDriver("Теодор Рузвельт", 33);
+        Mechanic mechanic = new Mechanic("Джорж Буш", "Tesla");
+        mechanic.addTransport(car2);
+        mechanic.repair(car2);
+        mechanic.performMaintenance(car2);
+        mechanic.addTransport(truck2);
+        mechanic.repair(truck2);
+
+        car2.setDriver(carDriver2);
+        car2.getDriver();
+        mechanic.getTransportList();
+
+
+//        BusDriver busDriver = new BusDriver("Теодор Рузвельт", 33);
+//        TruckDriver truckDriver = new TruckDriver("Уинстон Черчилль", -43);
         }
 }
 
