187ad8737d3596434380fc3f4757a7f65ed3633d
Dmitry M <a89297175356@gmail.com>
2023-07-12 18:29:35 +0500

    Lambdas1.3

--- src/Lambda/labmdas.java
+++ src/Lambda/labmdas.java
@@ -1,10 +1,13 @@
 package Lambda;
 
+import java.util.function.Consumer;
+import java.util.function.Function;
 import java.util.function.Predicate;
 
 public class labmdas {
     public static void main(String[] args) {
         int a = 5;
+        String f = "Pit";
         System.out.println("Задание 1. Является ли число положительным. Реализовать через анонимный класс через лямбду");
         Predicate<Integer> b = new Predicate<Integer>() {
             @Override
@@ -15,6 +18,26 @@ public class labmdas {
         System.out.println(b.test(a));
         Predicate<Integer> b1 = c -> c >= 0;
         System.out.println(b1.test(a));
-
+        Consumer<String> greetingConsumer1 = new Consumer<String>() {
+            @Override
+            public void accept(String name) {
+                System.out.println("Привет, " + name + "!");
+            }
+        };
+        greetingConsumer1.accept("Tit");
+        Consumer<String> greetingConsumer2 = name -> System.out.println("Привет, " + name + "!");
+        greetingConsumer2.accept("Pit");
+        Function<Double, Long> number = new Function<Double, Long>() {
+            @Override
+            public Long apply(Double number) {
+                return Math.round(number);
+            }
+        };
+        System.out.println(number.apply(2.2));
+        Function<Double, Long> number12 = number1 -> Math.round(number1);
+        System.out.println(number12.apply(3.3));
     }
 }
+
+
+
