c859a73cf9c403d9d794fe7757da09ded188d1ef
Dmitry M <a89297175356@gmail.com>
2023-08-03 16:20:10 +0500

    CourseWork2.11

--- src/CourseWork2/Subtask/SubTask.java
+++ src/CourseWork2/Subtask/SubTask.java
@@ -11,26 +11,24 @@ public class SubTask {
     /*1. Количество слов в тексте.
 2. Топ-10 самых часто упоминаемых слов, упорядоченных по количеству упоминаний в
 обратном порядке. В случае одинакового количества упоминаний слова должны быть отсортированы по алфавиту.
+Input:
 yourapp the quick brown fox jumps over the lazy dog  */
  public static void main(String[] args) {
     Scanner scanner = new Scanner(System.in);
-    String text = scanner.nextLine();
-   // ArrayList words = new ArrayList<>(text.split(" "));
+    String text = scanner.nextLine(); 
     String[] words = text.split(" ");
-
+    System.out.println("Количесто слов в строке " + words.length);
     List<String> list = Arrays.asList(words);
+    Map<String, Integer> frequency = list.stream()
+    .collect(Collectors.toMap(Function.identity(), e -> 1, Integer::sum));
+    System.out.println("Топ-10 самых часто упоминаемых слов:");
+    List<Map.Entry<String, Integer>> entries = frequency.entrySet().stream()
+        .sorted(Map.Entry.<String, Integer>comparingByValue().reversed())
+        .limit(10)
+        .collect(Collectors.toList());
+    for(Map.Entry<String, Integer> entry : entries){
+        System.out.println(entry.getKey() + ": " + entry.getValue());
+    }
 
-    Map<String, Integer> frequency = list.stream().collect(Collectors.toMap(null, null, null));
-         
-    System.out.println(frequency);
-    // System.out.println("В тексте " + words.length + " слов");
-    // for (String word : words) {
-    //     if(!word.equals(word)){
-            
-    //     }
-    //     System.out.println(word);
-    // }
-
-    // scanner.close();
  }
 }
