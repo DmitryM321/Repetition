f59c9e5d27d8545aa07dc847833710f9c15afc99
Dmitry M <a89297175356@gmail.com>
2023-08-04 11:28:46 +0500

    Stepik1.2

--- src/Stepik/Task.java
+++ src/Stepik/Task.java
@@ -4,19 +4,25 @@ import java.security.MessageDigest;
 
 public class Task {   
      public static void main(String[] args) throws Exception {
-        
-        int year =404;
-        System.out.println(isLeapYear(year));
+        int year =100;
+        System.out.println(leapYearCount(year));
      }
 
-        public static boolean isLeapYear(int year) {
-       
-        if (year % 4 == 0 && (year % 100 != 0 ^ year % 400 == 0)) {
-                return true;
-            }
+     public static int leapYearCount(int year) {
+            int sum = 0;
+             for (int i = 0; i < year; i++) {
+                if(year <=3){
+                return 0;
+                }
+               if (i % 4 == 0 && (i % 100 != 0 ^ i % 400 == 0)) {
+                     sum++;
+                }             
+        //   }  else  {
+        //         return 0;
+     }return sum;
+            
+    } 
         
-        return false;
     }
     
-}
 
