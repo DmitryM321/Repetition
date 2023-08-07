50f5e87b3b43dd490e296aa0c3d1de28e7c9cb03
Dmitry M <a89297175356@gmail.com>
2023-08-07 16:38:17 +0500

    Stepik1.9

--- src/Stepik/Task3.java
+++ src/Stepik/Task3.java
@@ -1,5 +1,7 @@
 package Stepik;
 
+import java.math.BigInteger;
+
 public class Task3 {
 public static void main(String[] args) {
     System.out.println(        'A' + "12");
@@ -8,13 +10,32 @@ public static void main(String[] args) {
        System.out.println(       "A" + ('\t' + '\u0003'));
         System.out.println(       "A" + '\u0003');
         String text = "Madam, I'm Adam!";
+  
+        System.out.println(isPalindrome(text));
+        System.out.println(factorial(6));
+    
 }
 // Реализуйте метод, проверяющий, является ли заданная строка палиндромом. 
 public static boolean isPalindrome(String text) {
-    text.replaceAll("[^A-Za-z1-9]+","");
-
-
-    return true;  
+    String text1 = text.replaceAll("[^A-Za-z0-9А-Яа-яЁё]","");
+    StringBuilder reverse  = new StringBuilder(text1).reverse();
+    return text1.equalsIgnoreCase(reverse.toString());
 }
-    
+// Реализуйте метод, вычисляющий факториал заданного натурального числа. Факториал N вычисляется как 1⋅2⋅...⋅N.   
+public static int factorial1(int value) {
+    int sum =1;
+    for (int index = 1; index <= value; index++) {
+        sum  = sum * index  ;
+    }
+
+
+    return  sum;  
+}
+public static BigInteger factorial(int value) {
+    BigInteger result = BigInteger.ONE;
+  for (int i = 1; i <= value; i++)
+     result = result.multiply(BigInteger.valueOf(i));
+  return result;
+
+}
 }
