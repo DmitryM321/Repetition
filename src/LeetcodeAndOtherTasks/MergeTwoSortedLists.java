e0bf9414431edb0736a1b954029885b078f2541b
Dmitry M <a89297175356@gmail.com>
2023-08-09 15:18:04 +0500

    Tasks6

--- src/LeetcodeAndOtherTasks/MergeTwoSortedLists.java
+++ src/LeetcodeAndOtherTasks/MergeTwoSortedLists.java
@@ -1,23 +1,78 @@
 package LeetcodeAndOtherTasks;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.LinkedList;
+import java.util.List;
 
 class MergeTwoSortedLists {
-    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
-        return null;
+    public static void main(String[] args) {
+        ListNode list1 = new ListNode(1);
+        list1.next = new ListNode(2);
+        list1.next.next = new ListNode(4);
+
+        ListNode list2 = new ListNode(1);
+        list2.next = new ListNode(3);
+        list2.next.next = new ListNode(4);
+
+        ListNode mergedList = mergeTwoLists(list1, list2);
+        int[] result = convertToListArray(mergedList);
+        System.out.println(Arrays.toString(result));
     }
 
-    public static void main(String[] args) {
-    LinkedList<Integer> list1 = new LinkedList<>(Arrays.asList(1, 2, 4));
-    LinkedList<Integer> list2 = new LinkedList<>(Arrays.asList(1, 3, 4));
+    public static ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        ListNode dummy = new ListNode(-1);
+        ListNode current = dummy;
+
+        while (list1 != null && list2 != null) {
+            if (list1.val <= list2.val) {
+                current.next = list1;
+                list1 = list1.next;
+            } else {
+                current.next = list2;
+                list2 = list2.next;
+            }
+            current = current.next;
+        }
+
+        if (list1 != null) {
+            current.next = list1;
+        }
+
+        if (list2 != null) {
+            current.next = list2;
+        }
+
+        return dummy.next;
+    }
+
+    public static int[] convertToListArray(ListNode head) {
+        List<Integer> list = new ArrayList<>();
+        ListNode current = head;
+        while (current != null) {
+            list.add(current.val);
+            current = current.next;
+        }
+        int[] array = new int[list.size()];
+        for (int i = 0; i < list.size(); i++) {
+            array[i] = list.get(i);
+        }
+        return array;
     }
 }
 
- class ListNode {
+class ListNode {
     int val;
     ListNode next;
+
     ListNode() {}
-    ListNode(int val) { this.val = val; }
-    ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+
+    ListNode(int val) {
+        this.val = val;
+    }
+
+    ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
 }
