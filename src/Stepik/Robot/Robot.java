260d4d1172d0988ed074b95a8186c815979a981b
Dmitry M <a89297175356@gmail.com>
2023-08-10 12:34:02 +0500

    Stepic1.14

--- src/Stepik/Robot/Robot.java
+++ src/Stepik/Robot/Robot.java
@@ -1,67 +1,119 @@
 package Robot;
 
 public class Robot {
-    private Direction direction;
-    private int x;
-    private int y;
+    private static Direction direction; // создаем поле энама
+    private int x; // поле начальное по оси Х
+    private int y; // поле начальное по оси У
 
-    public Robot(Direction direction, int x, int y) {
-        this.direction = direction;
-        this.x = x;
-        this.y = y;
+    public Robot(Direction direction, int x, int y) { // создаем конструктор
+        Robot.direction = direction; // направление робота
+        this.x = x; // текущая точка по оси х
+        this.y = y; // текущая точка по оси у
     }
 
     public static void main(String[] args) {
-        Robot robot = new Robot(Direction.UP, 0, 0);
-        int toX = 3;
-        int toY = 0;
-        moveRobot(robot, toX, toY);
+        Robot robot = new Robot(Direction.UP, 0, 0); // новый объект Robot с заданным положением и направлением
+        int toX = 3; // точка на оси х куда надо попасть
+        int toY = 0; // точка на оси у куда надо попасть
+        moveRobot(robot, toX, toY); // вызов метода куда мы передаем robot(направление и текущее положение) и 2
+                                    // точки направлнеия
         System.out.println(robot.getX()); // Вывод координат робота после выполнения метода
-        System.out.println(robot.getY());
+        System.out.println(robot.getY()); // Вывод координат робота после выполнения метода
+    }
 
-    }   
+    public static void moveRobot(Robot robot, int toX, int toY) { // создание метода
+        int currentX = robot.getX(); // создание переменной текущей точки по оси х
+        int currentY = robot.getY(); // создание переменной текущей точки по оси у
 
-    public static void moveRobot(Robot robot, int toX, int toY) {
-     while (robot.getX() != toX || robot.getY() != toY){
-        if (robot.getX() < toX){
-
+        while (currentX != toX || currentY != toY) { // пока текущее положение не равно заданному по осям х и у
+            if (currentX < toX) { // если по оси х текущее меньше заданного
+                while (robot.getDirection() != Direction.RIGHT) { // пока положение робота не равно направо
+                    robot.turnRight(); // робот совершает поворот направо
+                }
+                robot.stepForward(); // делается шаг
+                currentX++; // текущее значение увеличивается на 1
+            } else if (currentX > toX) { // если текущее положение больше заданного
+                while (robot.getDirection() != Direction.LEFT) { // // пока положение робота не равно налево
+                    robot.turnRight(); // робот совершает поворот налево
+                }
+                robot.stepForward(); // делается шаг
+                currentX--; // текущее значение уменьшается на 1
+            } else if (currentY < toY) {
+                while (robot.getDirection() != Direction.UP) {
+                    robot.turnRight();
+                }
+                robot.stepForward();
+                currentY++;
+            } else if (currentY > toY) {
+                while (robot.getDirection() != Direction.DOWN) {
+                    robot.turnRight();
+                }
+                robot.stepForward();
+                currentY--;
+            }
         }
-
-     }
-
-    public static Direction getDirection(Direction up) {
-        return Direction;
-        // текущее направление взгляда
     }
 
-    public int getX() {
+    private Direction getDirection() { // возвращает текущее направление робота.
+        return this.direction;
+    }
+
+    public int getX() { // возвращает начальное положение по оси х
         return x;
-        // текущая координата X
     }
 
-    public int getY() {
+    public int getY() { // возвращает начальное положение по оси х
         return y;
-        // текущая координата Y
     }
 
     public void turnLeft() {
-
-        // повернуться на 90 градусов против часовой стрелки
+        switch (direction) {
+            case UP:
+                direction = Direction.LEFT;
+                break;
+            case DOWN:
+                direction = Direction.RIGHT;
+                break;
+            case LEFT:
+                direction = Direction.DOWN;
+                break;
+            case RIGHT:
+                direction = Direction.UP;
+                break;
+        }
     }
 
     public void turnRight() {
-        // повернуться на 90 градусов по часовой стрелке
+        switch (direction) {
+            case UP:
+                direction = Direction.RIGHT;
+                break;
+            case DOWN:
+                direction = Direction.LEFT;
+                break;
+            case LEFT:
+                direction = Direction.UP;
+                break;
+            case RIGHT:
+                direction = Direction.DOWN;
+                break;
+        }
     }
 
     public void stepForward() {
-        if (Direction == Direction.RIGHT) {
-            x = x + 1;
+        switch (direction) {
+            case UP:
+                y++;
+                break;
+            case DOWN:
+                y--;
+                break;
+            case LEFT:
+                x--;
+                break;
+            case RIGHT:
+                x++;
+                break;
         }
-        if (Direction == Direction.UP) {
-            y = y + 1;
-        }
-
-        // шаг в направлении взгляда
-        // за один шаг робот изменяет одну свою координату на единицу
     }
 }
