3d0f2fcb5dc4d7287ba8a185d004f225bc70c5cb
Dmitry M <a89297175356@gmail.com>
2023-08-18 13:11:13 +0500

    Stepik1.17

---
+++ src/Stepik/Errors.java
@@ -0,0 +1,12 @@
+package Stepik;
+
+public class Errors {
+    public static void main(String[] args) {
+        double x = 4;
+        System.out.println(sqrt(x));
+
+    }
+    public static double sqrt(double x) {
+        return 0; // your implementation here
+    }
+}
