dd9e8ad0adcaff9288d02d7f033420ebe1cc6af0
Dmitry M <a89297175356@gmail.com>
2023-08-18 15:05:16 +0500

    Stepk1.17

--- src/Stepik/Errors.java
+++ src/Stepik/Errors.java
@@ -2,11 +2,15 @@ package Stepik;
 
 public class Errors {
     public static void main(String[] args) {
-        double x = 4;
+        double x =  4;
         System.out.println(sqrt(x));
 
     }
     public static double sqrt(double x) {
-        return 0; // your implementation here
+      if(x < 0){
+        throw new IllegalArgumentException("Expected non-negative number, got " + x); 
+    }
+           
+        return Math.sqrt(x);  
     }
 }
