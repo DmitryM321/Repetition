29d974e191884aba90a35722dd5e94cddc5f091c
DmitryM321 <a89297175356@gmail.com>
2023-08-31 18:05:01 +0500

    Leetcode

--- src/LeetcodeAndOtherTasks/LengthofLastWord.java
+++ src/LeetcodeAndOtherTasks/LengthofLastWord.java
@@ -1,28 +1,39 @@
 package LeetcodeAndOtherTasks;
 
+import java.util.Arrays;
+
 public class LengthofLastWord {
 // Given a string s consisting of words and spaces, return the length of the last word in the string.
 public static void main(String[] args) {
-    String s = "Hello World";
+    String s = "luffy is still joyboy";
     System.out.println(lengthOfLastWord(s));
 }
 
 
+// public static int lengthOfLastWord(String s) {
+//     int length = 0;
+//     for (int i = s.length() - 1; i >= 0; i--) {
+//         if (s.charAt(i) == ' ') {
+//             if (length > 0) {
+//                 return length;
+//             }
+//         } else {
+//             length++;
+//         }
+//     }
+//     return length;
+// }
+
+
 public static int lengthOfLastWord(String s) {
-    s.split(" ");
-    char[] word = s.toCharArray();
-    int count = 0;
-    for (int i = s.length(); i > s.length() - 1; i--) {
-    }  
-    return 0;
-}
-public static int lengthOfLastWord(String s) {
-    s.split(" ");
-    char[] word = s.toCharArray();
-    int count = 0;
-    for (int i = s.length(); i > s.length() - 1; i--) {
-    }  
-    return 0;
+    String[] words = s.split(" ");
+int count = Arrays.stream(words)
+.mapToInt(word -> word.length())
+.reduce((a, b) -> b)
+.orElse(0);
+
+return count;
+
 }
  
 
