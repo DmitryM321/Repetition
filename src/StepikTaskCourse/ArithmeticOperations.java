986d07a39f4129afe3ab18a1f9f1203b01c4b422
DmitryM321 <a89297175356@gmail.com>
2023-09-04 15:58:50 +0500

    StepiKTask

---
+++ src/StepikTaskCourse/ArithmeticOperations.java
@@ -0,0 +1,28 @@
+package StepikTaskCourse;
+import java.util.Scanner;
+public class ArithmeticOperations {
+
+    public static void main(String[] args) {
+       // Дано два числа a и b. Найдите гипотенузу треугольника с заданными катетами.
+        int a1 = 6;
+        int b1 = 8;
+        int c1 = (int) Math.sqrt((a1*a1) + (b1*b1));
+        System.out.println(c1);
+// считывает целое число и выводит текст, аналогичный приведенному в примере.
+// Пробелы, знаки препинания, заглавные и строчные буквы важны179
+
+// Scanner scanner = new Scanner(System.in);
+// int a2 = scanner.nextInt();
+// int b2 = a2 +1;
+// int c2 = a2 - 1;
+// System.out.println("The next number for the number " + a2 +" is " + b2 + ".");
+// System.out.println("The previous number for the number " + a2 +" is " + c2 + ".");
+
+// Программа получает на вход числа N и K. должна вывести искомое количество яблок.
+Scanner scanner = new Scanner(System.in);
+ int a3 = scanner.nextInt();
+
+
+
+    }
+}
