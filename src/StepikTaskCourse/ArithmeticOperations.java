d45df6fb83a1a3eb3ea9577a6de6f021aa8a94ad
DmitryM321 <a89297175356@gmail.com>
2023-09-05 17:50:28 +0500

    StepilTask1.4

--- src/StepikTaskCourse/ArithmeticOperations.java
+++ src/StepikTaskCourse/ArithmeticOperations.java
@@ -1,4 +1,6 @@
 package StepikTaskCourse;
+import java.lang.reflect.Array;
+import java.util.Arrays;
 import java.util.Scanner;
 public class ArithmeticOperations {
 
@@ -29,21 +31,47 @@ public class ArithmeticOperations {
 //  109 километров. Байкер Вася стартует с нулевого километра МКАД и едет со скоростью v километров в час.
 // На какой отметке он остановится через t часов?Программа должна вывести целое число 
 // от 0 до 108 — номер отметки, на которой остановится Вася.
-Scanner scanner = new Scanner(System.in);
- int v = scanner.nextInt();
- int t = scanner.nextInt();   
-int circle = 109;
-int fin = 0;
 
-if(v > 0) {
-    fin = (v * t) % circle;
-} else {
-    fin = Math.abs((v * t) % circle);
-    if(fin == 0) {
-        fin = 0;
-    } else {
-        fin = circle - fin;
-    }
-} 
-        System.out.println(fin);
+//  int v = scanner.nextInt();
+//  int t = scanner.nextInt();   
+// int circle = 109;
+// int fin = 0;
+
+// if(v > 0) {
+//     fin = (v * t) % circle;
+// } else {
+//     fin = Math.abs((v * t) % circle);
+//     if(fin == 0) {
+//         fin = 0;
+//     } else {
+//         fin = circle - fin;
+//     }
+// } 
+//         System.out.println(fin);
+// Scanner scanner = new Scanner(System.in);
+// int a4 = 345;
+// String b4 = Integer.toString(a4);
+// char[] c4 = b4.toCharArray();
+
+
+//  System.out.println(c4[c4.length-1]);
+// Scanner scanner = new Scanner(System.in);
+// int number = scanner.nextInt();
+// int tens = number / 10;   
+// tens = tens % 10;  
+// System.out.println(tens);
+
+// Дано трехзначное число (оно может быть отрицательным). Найдите сумму его цифр.
+Scanner scanner = new Scanner(System.in);
+int number = scanner.nextInt();
+int sum = 0;
+int absNumber = Math.abs(number);
+
+while (absNumber > 0) {
+    int digit = absNumber % 10;  
+    sum += digit; 
+    absNumber /= 10;  
+}
+
+System.out.println(sum);
     }}
