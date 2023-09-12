44377e70bed70a41d2ae19c4254405c8735d9bfb
DmitryM321 <a89297175356@gmail.com>
2023-09-12 15:07:31 +0500

    stepikask

--- src/StepikTaskCourse/ArithmeticOperations2.java
+++ src/StepikTaskCourse/ArithmeticOperations2.java
@@ -34,19 +34,34 @@ public class ArithmeticOperations2 {
 //     System.out.println(5%7);
 // Улитка ползёт по вертикальному шесту высотой h метров, поднимаясь за день на a метров, а за ночь 
 //спускаясь на b метров. На какой день улитка доползёт до вершины шеста?
-Scanner scanner = new Scanner(System.in);
+//Scanner scanner = new Scanner(System.in);
 //      int h = scanner.nextInt();
 //        int a = scanner.nextInt();
 //      int b = scanner.nextInt();
 
 
-int h = 100;  // 8 
-  int a = 3;
-    int b = 2;
-    int climbed = a - b;
-    int days = (h - a + climbed - 1) / climbed + 1;
+// int h = 100;  // 8 
+//   int a = 3;
+//     int b = 2;
+//     int climbed = a - b;
+//     int days = (h - a + climbed - 1) / climbed + 1;
 
-    System.out.println(days);
+//     System.out.println(days);
+        Scanner scanner = new Scanner(System.in);
+        int number = scanner.nextInt();
 
+    int thousandsPlace = number / 1000;
+    int hundredsPlace = (number / 100) % 10;
+    int tensPlace = (number / 10) % 10;
+    int unitsPlace = number % 10;
+
+    int sum = (thousandsPlace + unitsPlace) - (hundredsPlace + tensPlace);
+
+    if (sum == 0) {
+        System.out.println(1);
+    } else {
+        System.out.println(0);
     }
 }
+}
+    
